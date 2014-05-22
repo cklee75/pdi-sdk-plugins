752c09c34fb2914d9b445f751e3f4acccf504e44
CK <cklee7755@gmail.com>
2014-05-22 15:50:56 +0800

    code cleanup

--- kettle-sdk-jobentry-plugin/src/org/pentaho/di/sdk/samples/jobentries/demo/JobEntryDemoDialog.java
+++ kettle-sdk-jobentry-plugin/src/org/pentaho/di/sdk/samples/jobentries/demo/JobEntryDemoDialog.java
@@ -25,38 +25,18 @@ package org.pentaho.di.sdk.samples.jobentries.demo;
 import java.util.Arrays;
 import java.util.List;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CCombo;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.ShellAdapter;
-import org.eclipse.swt.events.ShellEvent;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-import org.pentaho.di.core.Const;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.job.JobHopMeta;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.job.entries.columnsexist.JobEntryColumnsExist;
-import org.pentaho.di.job.entry.JobEntryCopy;
 import org.pentaho.di.job.entry.JobEntryDialogInterface;
 import org.pentaho.di.job.entry.JobEntryInterface;
 import org.pentaho.di.repository.Repository;
-import org.pentaho.di.ui.core.gui.WindowProperty;
+import org.pentaho.di.ui.core.PropsUI;
 import org.pentaho.di.ui.job.dialog.JobDialog;
 import org.pentaho.di.ui.job.entry.JobEntryDialog;
-import org.pentaho.di.ui.trans.step.BaseStepDialog;
 
 
 /**
@@ -84,18 +64,24 @@ public class JobEntryDemoDialog extends JobEntryDialog implements JobEntryDialog
 	 */		
 	private static Class<?> PKG = JobEntryDemo.class; // for i18n purposes
 
-	// the text box for the job entry name
-    private Text wName;
-    // the combo box for the outcomes
-    private CCombo wOutcome;
-
     // the job entry configuration object
     private JobEntryDemo meta;
-    
-    // flag saving the changed status of the job entry configuration object
-    private boolean changed;
 	
     /**
+	 * @return the pKG
+	 */
+	public static Class<?> getPKG() {
+		return PKG;
+	}
+
+	/**
+	 * @return the meta
+	 */
+	public JobEntryDemo getMeta() {
+		return meta;
+	}
+
+	/**
      * The constructor should call super() and make sure that the name of the job entry is set.  
      * 
      * @param parent		the SWT Shell to use
@@ -108,7 +94,11 @@ public class JobEntryDemoDialog extends JobEntryDialog implements JobEntryDialog
         // it is safe to cast the JobEntryInterface object to the object handled by this dialog
         meta = (JobEntryDemo) jobEntryInt;
         // ensure there is a default name for new job entries
-        List<JobEntryCopy> jobEntryCopies =  jobMeta.getJobCopies();
+        if (this.meta.getName() == null){
+            this.meta.setName(BaseMessages.getString(PKG, "Demo.Default.Name"));
+        }
+
+        // TEMP, example how to retrieve incoming/outgoing entries.
         List<JobHopMeta> jobHopMetas = jobMeta.getJobhops();
         for (JobHopMeta jobHopMeta : jobHopMetas) {
         	if (jobHopMeta.getToEntry() != null && 
@@ -131,17 +121,7 @@ public class JobEntryDemoDialog extends JobEntryDialog implements JobEntryDialog
         		}
         	}
         }
-        /*int posistion = 0;
-        for (JobEntryCopy jobEntryCopy : jobEntryCopies) {
-        	if (jobEntryCopy.getEntry().equals(jobEntryInt)) {
-        		break;
-        	}
-        	++posistion;
-        }
-        System.out.println("Previous entry: " + jobMeta.getJobEntry(posistion-1).getName());*/
-        if (this.meta.getName() == null){
-            this.meta.setName(BaseMessages.getString(PKG, "Demo.Default.Name"));
-        }
+
     }
     
 	/**
@@ -158,199 +138,15 @@ public class JobEntryDemoDialog extends JobEntryDialog implements JobEntryDialog
 	 * The open() method must return the met object of the job entry after the user has confirmed the dialog,
 	 * or null if the user cancelled the dialog.
 	 */
-    public JobEntryInterface open_(){
-
-    	// SWT code for setting up the dialog
-        Shell parent = getParent();
-        Display display = parent.getDisplay();
-
-        shell = new Shell(parent, props.getJobsDialogStyle());
-        props.setLook(shell);
-        JobDialog.setShellImage(shell, meta);
-        
-        // save the job entry's changed flag
-        changed = meta.hasChanged();
-
-        // The ModifyListener used on all controls. It will update the meta object to 
-		// indicate that changes are being made.
-        ModifyListener lsMod = new ModifyListener(){
-            public void modifyText(ModifyEvent e){
-                meta.setChanged();
-            }
-        };
-
-        // ------------------------------------------------------- //
-		// SWT code for building the actual settings dialog        //
-		// ------------------------------------------------------- //
-        FormLayout formLayout = new FormLayout();
-        formLayout.marginWidth = Const.FORM_MARGIN;
-        formLayout.marginHeight = Const.FORM_MARGIN;
-
-        shell.setLayout(formLayout);
-        shell.setText(BaseMessages.getString(PKG, "Demo.Shell.Title"));
-
-        int middle = props.getMiddlePct();
-        int margin = Const.MARGIN;
-
-        // Job entry name line
-        Label wlName = new Label(shell, SWT.RIGHT);
-        wlName.setText(BaseMessages.getString(PKG, "Demo.JobEntryName.Label"));
-        props.setLook(wlName);
-        FormData fdlName = new FormData();
-        fdlName.left = new FormAttachment(0, 0);
-		fdlName.right = new FormAttachment(middle, 0);
-        fdlName.top = new FormAttachment(0, margin);
-        wlName.setLayoutData(fdlName);
-        wName = new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-        props.setLook(wName);
-        wName.addModifyListener(lsMod);
-        FormData fdName = new FormData();
-        fdName.left = new FormAttachment(middle, 0);
-        fdName.top = new FormAttachment(0, margin);
-        fdName.right = new FormAttachment(100, 0);
-        wName.setLayoutData(fdName);
-
-        // Outcome
-        Label wlOutcome = new Label(shell, SWT.RIGHT);
-        wlOutcome.setText(BaseMessages.getString(PKG, "Demo.Outcome.Label"));
-        props.setLook(wlOutcome);
-        FormData fdlOutcome = new FormData();
-        fdlOutcome.left = new FormAttachment(0, 0);
-        fdlOutcome.right = new FormAttachment(middle, -margin);
-        fdlOutcome.top = new FormAttachment(wName, margin);
-        wlOutcome.setLayoutData(fdlOutcome);
-        wOutcome = new CCombo(shell, SWT.SINGLE | SWT.READ_ONLY | SWT.BORDER);
-        wOutcome.setItems(new String[]{"Positive","Negative"});
-        wOutcome.setEditable(false);
-        wOutcome.select(0);
-        wOutcome.addModifyListener(lsMod);
-        props.setLook(wOutcome);
-        FormData fdOutcome = new FormData();
-        fdOutcome.left = new FormAttachment(middle, 0);
-        fdOutcome.top = new FormAttachment(wName, margin);
-        fdOutcome.right = new FormAttachment(100, 0);
-        //fdOutcome.bottom = new FormAttachment(100, -40);
-        wOutcome.setLayoutData(fdOutcome);
-
-        Button wOK = new Button(shell, SWT.PUSH);
-        wOK.setText(BaseMessages.getString(PKG, "System.Button.OK"));
-        Button wCancel = new Button(shell, SWT.PUSH);
-        wCancel.setText(BaseMessages.getString(PKG, "System.Button.Cancel"));
-
-        // at the bottom
-        BaseStepDialog.positionBottomButtons(shell, new Button[] { wOK, wCancel }, margin, null);
-
-        // Add listeners
-        Listener lsCancel = new Listener(){
-            public void handleEvent(Event e){cancel();}
-        };
-
-        Listener lsOK = new Listener(){
-            public void handleEvent(Event e){ok();}
-        };
-        
-        wCancel.addListener(SWT.Selection, lsCancel);
-        wOK.addListener(SWT.Selection, lsOK);
-
-        // default listener (for hitting "enter")
-        SelectionAdapter lsDef = new SelectionAdapter(){
-            public void widgetDefaultSelected(SelectionEvent e){ok();}
-        };
-
-        wName.addSelectionListener(lsDef);
-
-		// Detect X or ALT-F4 or something that kills this window and cancel the dialog properly
-        shell.addShellListener(new ShellAdapter(){
-            public void shellClosed(ShellEvent e){
-                cancel();
-            }
-        });
-        
-		// populate the dialog with the values from the meta object
-        populateDialog();
-
-        // restore the changed flag to original value, as the modify listeners fire during dialog population 
-        meta.setChanged(changed);
-        
-        // restore dialog size and placement, or set default size if none saved yet 
-        BaseStepDialog.setSize(shell, 250, 140, false);
-        
-		// open dialog and enter event loop 
-        shell.open();
-        while (!shell.isDisposed()){
-            if (!display.readAndDispatch()){
-            	display.sleep();
-            }
-        }
-		// at this point the dialog has closed, so either ok() or cancel() have been executed
-        return meta;
-    }
-
     public JobEntryInterface open(){
     	// SWT code for setting up the dialog
         Shell parent = getParent();
         Display display = parent.getDisplay();
 
 		JobEntryDemoShell jobEntryDemoShell = new JobEntryDemoShell(parent,
-				props.getJobsDialogStyle(), PKG, meta, props.getMiddlePct(), changed);
+				props.getJobsDialogStyle(), this);
         shell = jobEntryDemoShell;
-        props.setLook(shell);
         JobDialog.setShellImage(shell, meta);
-
-        wName = jobEntryDemoShell.getWName();
-        wOutcome = jobEntryDemoShell.getWOutcome();
-        
-        // save the job entry's changed flag
-        changed = meta.hasChanged();
-        
-        // The ModifyListener used on all controls. It will update the meta object to 
-		// indicate that changes are being made.
-        ModifyListener lsMod = new ModifyListener(){
-            public void modifyText(ModifyEvent e){
-                meta.setChanged();
-            }
-        };
-        
-//        int middle = props.getMiddlePct();
-        int margin = Const.MARGIN;
-
-        props.setLook(wName);
-        wName.addModifyListener(lsMod);
-        
-        wOutcome.addModifyListener(lsMod);
-		props.setLook(wOutcome);
-		
-		props.setLook(jobEntryDemoShell.getWlName());
-		props.setLook(jobEntryDemoShell.getWlOutcome());
-        
-		// at the bottom
-//		BaseStepDialog.positionBottomButtons(
-//				shell,
-//				new Button[] { jobEntryDemoShell.getBtnOk(),
-//						jobEntryDemoShell.getBtnCancel() }, margin, null);
-
-        // default listener (for hitting "enter")
-        /*SelectionAdapter lsDef = new SelectionAdapter(){
-            public void widgetDefaultSelected(SelectionEvent e){ok();}
-        };
-
-        jobEntryDemoShell.getWName().addSelectionListener(lsDef);*/
-
-		// Detect X or ALT-F4 or something that kills this window and cancel the dialog properly
-        shell.addShellListener(new ShellAdapter(){
-            public void shellClosed(ShellEvent e){
-                cancel();
-            }
-        });
-        
-		// populate the dialog with the values from the meta object
-        populateDialog();
-
-        // restore the changed flag to original value, as the modify listeners fire during dialog population 
-        meta.setChanged(changed);
-        
-        // restore dialog size and placement, or set default size if none saved yet 
-        //BaseStepDialog.setSize(shell, 250, 140, false);
         
 		// open dialog and enter event loop 
         shell.open();
@@ -359,72 +155,18 @@ public class JobEntryDemoDialog extends JobEntryDialog implements JobEntryDialog
             	display.sleep();
             }
         }
+        
 		// at this point the dialog has closed, so either ok() or cancel() have been executed
         return meta;
     }    
     
     /**
-     * This helper method is called once the dialog is closed. It saves the placement of
-     * the dialog, so it can be restored when it is opened another time.
+     * Expose protected props field to be used by SWT shell.
+     * 
+     * @return
      */
-    private void dispose(){
-    	// save dialog window placement to use when reopened 
-        WindowProperty winprop = new WindowProperty(shell);
-        props.setScreen(winprop);
-        // close dialog window
-        shell.dispose();
+    public PropsUI getPropsUI() {
+    	return this.props;
     }
-
-    /**
-     * Copy information from the meta-data input to the dialog fields.
-     */
-    public void populateDialog(){
-
-    	// setting the name of the job entry
-    	if (meta.getName() != null){
-            wName.setText(meta.getName());
-        }
-        wName.selectAll();
-        
-        // choosing the configured value for the outcome on the selector box
-        wOutcome.select(meta.getOutcome()?0:1);
-        
-    }
-
-    /**
-     * This method is called once the dialog has been canceled.
-     */
-    private void cancel(){
-    	// restore changed flag on the meta object, any changes done by the modify listener
-    	// are being revoked here
-        meta.setChanged(changed);
-        // this variable will be returned by the open() method, setting it to null, as open() needs
-        // to return null when the dialog is cancelled
-        meta = null;
-        // close dialog window and clean up
-        dispose();
-    }
-
-    /**
-     * This method is called once the dialog is confirmed. It may only close the window if the 
-     * job entry has a non-empty name.
-     */
-    private void ok(){
-
-    	// make sure the job entry name is set properly, return with an error message if that is not the case
-    	if(Const.isEmpty(wName.getText())) {
-    		MessageBox mb = new MessageBox(shell, SWT.OK | SWT.ICON_ERROR );
-    		mb.setText(BaseMessages.getString(PKG, "System.StepJobEntryNameMissing.Title"));
-    		mb.setMessage(BaseMessages.getString(PKG, "System.JobEntryNameMissing.Msg"));
-    		mb.open();
-    		return;
-    	}
-    	
-    	// update the meta object with the entered dialog settings
-    	meta.setName(wName.getText());
-    	meta.setOutcome(wOutcome.getSelectionIndex()==0);
-
-    	// close dialog window and clean up
-        dispose();
-    }
+    
 }
--- kettle-sdk-jobentry-plugin/src/org/pentaho/di/sdk/samples/jobentries/demo/JobEntryDemoShell.java
+++ kettle-sdk-jobentry-plugin/src/org/pentaho/di/sdk/samples/jobentries/demo/JobEntryDemoShell.java
@@ -2,6 +2,8 @@ package org.pentaho.di.sdk.samples.jobentries.demo;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -17,20 +19,26 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.ui.core.PropsUI;
+import org.pentaho.di.ui.core.gui.WindowProperty;
 import org.eclipse.swt.graphics.Point;
 
 public class JobEntryDemoShell extends Shell {
 	
 	private Button btnOk;
 	private Button btnCancel;
-	private Class<?> PKG;
-	private JobEntryDemo meta;
 	private Text wName;
 	private CCombo wOutcome;
-	private int middle;
 	private Label wlOutcome;
 	private Label wlName;
+
+    // flag saving the changed status of the job entry configuration object
 	private boolean changed;
+
+	// From Pentaho
+	private Class<?> PKG;
+	private JobEntryDemo meta;
+	private PropsUI props;
 
 	/**
 	 * Launch the application.
@@ -56,14 +64,12 @@ public class JobEntryDemoShell extends Shell {
 	 * @param parent
 	 * @param arg1
 	 */
-	public JobEntryDemoShell(final Shell parent, final int arg1, final Class<?> PKG,
-			final JobEntryDemo jobEntryDemo, final int middle, final boolean changed) {
+	public JobEntryDemoShell(final Shell parent, final int arg1, final JobEntryDemoDialog dialog) {
 		super(parent, arg1);
-		setMinimumSize(new Point(250, 100));
-		this.PKG = PKG;
-		this.meta = jobEntryDemo;
-		this.middle = middle;
-		this.changed = changed;
+		setMinimumSize(new Point(350, 200));
+		this.PKG = JobEntryDemoDialog.getPKG();
+		this.meta = dialog.getMeta();
+		this.props = dialog.getPropsUI();
 
         createContents();
 		FormLayout formLayout = new FormLayout();
@@ -76,7 +82,6 @@ public class JobEntryDemoShell extends Shell {
 		btnOk = new Button(this, SWT.NONE);
 		btnOk.addMouseListener(lsOk(parent, PKG));
 		FormData fd_btnOk = new FormData();
-		//fd_btnOk.width = 50;
 		fd_btnOk.left = new FormAttachment(25);
 		fd_btnOk.bottom = new FormAttachment(100);
 		btnOk.setLayoutData(fd_btnOk);
@@ -85,32 +90,27 @@ public class JobEntryDemoShell extends Shell {
 		btnCancel = new Button(this, SWT.NONE);
 		btnCancel.addMouseListener(lsCancel(changed));
 		FormData fd_btnCancel = new FormData();
-		//fd_btnCancel.width = 50;
 		fd_btnCancel.right = new FormAttachment(75);
 		fd_btnCancel.bottom = new FormAttachment(100);
 		btnCancel.setLayoutData(fd_btnCancel);
 		btnCancel.setText(BaseMessages.getString(PKG, "System.Button.Cancel"));
 		
-       //int middle = props.getMiddlePct();
-		//int middle = 1;
+		int mdl = Const.MIDDLE_PCT;
         int margin = Const.MARGIN;
 		
         // Job entry name line
         wlName = new Label(this, SWT.RIGHT);
         wlName.setText(BaseMessages.getString(PKG, "Demo.JobEntryName.Label"));
-        //props.setLook(wlName);
         FormData fd_wlName = new FormData();
         fd_wlName.left = new FormAttachment(0, 0);
-		fd_wlName.right = new FormAttachment(middle, 0);
+		fd_wlName.right = new FormAttachment(mdl, 0);
         fd_wlName.top = new FormAttachment(0, margin);
         wlName.setLayoutData(fd_wlName);
         
         wName = new Text(this, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
         wName.addSelectionListener(new WNameSelectionListener(this));
-//        props.setLook(wName);
-//        wName.addModifyListener(lsMod);
         FormData fdName = new FormData();
-        fdName.left = new FormAttachment(middle, 0);
+        fdName.left = new FormAttachment(wlName, margin);
         fdName.top = new FormAttachment(0, margin);
         fdName.right = new FormAttachment(100, 0);
         wName.setLayoutData(fdName);
@@ -118,27 +118,62 @@ public class JobEntryDemoShell extends Shell {
         // Outcome
         wlOutcome = new Label(this, SWT.RIGHT);
         wlOutcome.setText(BaseMessages.getString(PKG, "Demo.Outcome.Label"));
-//        props.setLook(wlOutcome);
         FormData fd_wlOutcome = new FormData();
         fd_wlOutcome.left = new FormAttachment(0, 0);
-        fd_wlOutcome.right = new FormAttachment(middle, -margin);
+        fd_wlOutcome.right = new FormAttachment(mdl, 0);
         fd_wlOutcome.top = new FormAttachment(wName, margin);
         wlOutcome.setLayoutData(fd_wlOutcome);
         wOutcome = new CCombo(this, SWT.SINGLE | SWT.READ_ONLY | SWT.BORDER);
         wOutcome.setItems(new String[]{"Positive","Negative"});
         wOutcome.setEditable(false);
         wOutcome.select(0);
-//        wOutcome.addModifyListener(lsMod);
-//        props.setLook(wOutcome);
         FormData fdOutcome = new FormData();
-        fdOutcome.left = new FormAttachment(middle, 0);
+        fdOutcome.left = new FormAttachment(wlOutcome, margin);
         fdOutcome.top = new FormAttachment(wName, margin);
         fdOutcome.right = new FormAttachment(100, 0);
-        //fdOutcome.bottom = new FormAttachment(100, -40);
         wOutcome.setLayoutData(fdOutcome);
 
+        props.setLook(wName);
+		props.setLook(wOutcome);
+		props.setLook(wlName);
+		props.setLook(wlOutcome);
+        props.setLook(this);
+
+        // save the job entry's changed flag
+        changed = meta.hasChanged();
+        
+        // The ModifyListener used on all controls. It will update the meta object to 
+		// indicate that changes are being made.
+        ModifyListener lsMod = new ModifyListener(){
+            public void modifyText(ModifyEvent e){
+                meta.setChanged();
+            }
+        };
+        
+        wName.addModifyListener(lsMod);
+        wOutcome.addModifyListener(lsMod);
+        
+        // restore the changed flag to original value, as the modify listeners fire during dialog population 
+        meta.setChanged(changed);
+        
+        populateDialog();
 	}
 
+    /**
+     * Copy information from the meta-data input to the dialog fields.
+     */
+    private void populateDialog(){
+
+    	// setting the name of the job entry
+    	if (meta.getName() != null){
+            wName.setText(meta.getName());
+        }
+        wName.selectAll();
+        
+        // choosing the configured value for the outcome on the selector box
+        wOutcome.select(meta.getOutcome()?0:1);
+    }
+	
 	/**
 	 * @param changed
 	 * @return
@@ -154,7 +189,7 @@ public class JobEntryDemoShell extends Shell {
 		        // to return null when the dialog is cancelled
 		        meta = null;
 		        // close dialog window and clean up
-		        dispose();
+		        cleanUp();
 			}
 		};
 	}
@@ -168,21 +203,7 @@ public class JobEntryDemoShell extends Shell {
 		return new MouseAdapter() {
 			@Override
 			public void mouseUp(MouseEvent arg0) {
-		    	// make sure the job entry name is set properly, return with an error message if that is not the case
-		    	if(Const.isEmpty(wName.getText())) {
-		    		MessageBox mb = new MessageBox(parent, SWT.OK | SWT.ICON_ERROR );
-		    		mb.setText(BaseMessages.getString(PKG, "System.StepJobEntryNameMissing.Title"));
-		    		mb.setMessage(BaseMessages.getString(PKG, "System.JobEntryNameMissing.Msg"));
-		    		mb.open();
-		    		return;
-		    	}
-		    	
-		    	// update the meta object with the entered dialog settings
-		    	meta.setName(wName.getText());
-		    	meta.setOutcome(wOutcome.getSelectionIndex()==0);
-
-		    	// close dialog window and clean up
-		        dispose();
+				ok(parent);
 			}
 		};
 	}
@@ -197,29 +218,43 @@ public class JobEntryDemoShell extends Shell {
 		this.setLocation((screenSize.width - this.getBounds().width) / 2, (screenSize.height - this.getBounds().height) / 2);
 	}
 
+    /**
+     * This helper method is called once the dialog is closed. It saves the placement of
+     * the dialog, so it can be restored when it is opened another time.
+     */
+    private void cleanUp(){
+    	// save dialog window placement to use when reopened 
+        WindowProperty winprop = new WindowProperty(this);
+        
+        props.setScreen(winprop);
+        // close dialog window
+        this.dispose();
+    }
+
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
 	}
-	public Button getBtnOk() {
-		return btnOk;
-	}
-	public Button getBtnCancel() {
-		return btnCancel;
-	}
-	public CCombo getWOutcome() {
-		return wOutcome;
-	}
-	public Text getWName() {
-		return wName;
-	}
-	public Label getWlOutcome() {
-		return wlOutcome;
-	}
-	public Label getWlName() {
-		return wlName;
-	}
 
+	private void ok(final Shell parent) {
+    	// make sure the job entry name is set properly, return with an error message if that is not the case
+    	if(Const.isEmpty(wName.getText())) {
+    		MessageBox mb = new MessageBox(parent, SWT.OK | SWT.ICON_ERROR );
+    		mb.setText(BaseMessages.getString(PKG, "System.StepJobEntryNameMissing.Title"));
+    		mb.setMessage(BaseMessages.getString(PKG, "System.JobEntryNameMissing.Msg"));
+    		mb.open();
+    		return;
+    	}
+    	
+    	// update the meta object with the entered dialog settings
+    	meta.setName(wName.getText());
+    	meta.setOutcome(wOutcome.getSelectionIndex()==0);
+
+    	// close dialog window and clean up
+    	cleanUp();
+
+	}
+	
 	private class WNameSelectionListener extends SelectionAdapter {
 		
 		private final Shell parent;
@@ -234,21 +269,49 @@ public class JobEntryDemoShell extends Shell {
 
 		@Override
 		public void widgetDefaultSelected(final SelectionEvent arg0) {
-	    	// make sure the job entry name is set properly, return with an error message if that is not the case
-	    	if(Const.isEmpty(wName.getText())) {
-	    		MessageBox mb = new MessageBox(parent, SWT.OK | SWT.ICON_ERROR );
-	    		mb.setText(BaseMessages.getString(PKG, "System.StepJobEntryNameMissing.Title"));
-	    		mb.setMessage(BaseMessages.getString(PKG, "System.JobEntryNameMissing.Msg"));
-	    		mb.open();
-	    		return;
-	    	}
-	    	
-	    	// update the meta object with the entered dialog settings
-	    	meta.setName(wName.getText());
-	    	meta.setOutcome(wOutcome.getSelectionIndex()==0);
-
-	    	// close dialog window and clean up
-	        dispose();
+	    	ok(parent);
 		}
+	}
+	
+	/**
+	 * @return
+	 */
+	public Button getBtnOk() {
+		return btnOk;
+	}
+	
+	/**
+	 * @return
+	 */
+	public Button getBtnCancel() {
+		return btnCancel;
+	}
+	
+	/**
+	 * @return
+	 */
+	public CCombo getWOutcome() {
+		return wOutcome;
+	}
+	
+	/**
+	 * @return
+	 */
+	public Text getWName() {
+		return wName;
+	}
+	
+	/**
+	 * @return
+	 */
+	public Label getWlOutcome() {
+		return wlOutcome;
+	}
+	
+	/**
+	 * @return
+	 */
+	public Label getWlName() {
+		return wlName;
 	}
 }
